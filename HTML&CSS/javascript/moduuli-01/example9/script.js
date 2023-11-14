88b4bdb4130835c5a160683f2655ec8a63d8f2da
susulmtnn <suvi.liimatainen@gmail.com>
2023-11-14 20:58:46 +0200

    completed 9

--- HTML&CSS/javascript/moduuli-01/example9/script.js
+++ HTML&CSS/javascript/moduuli-01/example9/script.js
@@ -2,7 +2,18 @@
 
 const userInput = parseInt(prompt("Please give a number to test for Prime numbers"));
 
-for (let i = 0; i <=userInput; i++)
-{
+const selectedSlot = document.querySelector('.slot');
 
+for (let i = 2; i <=userInput; i++)
+{
+    if (userInput % i === 0 && userInput !== i)
+    {
+      selectedSlot.innerHTML = `${userInput} is divisible by other numbers than itself and therefore it is not a prime.`;
+      break;
+    }
+    else if (i === userInput)
+    {
+      selectedSlot.innerHTML = `${userInput} is divisible only by 1 and itself and therefore it is a prime.`;
+
+    }
 }
