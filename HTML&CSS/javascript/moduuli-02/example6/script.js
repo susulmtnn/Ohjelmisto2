ea6527350faace1d94b7112adbe4526d6627e16c
susulmtnn <suvi.liimatainen@gmail.com>
2023-11-18 22:38:33 +0200

    completed example 6

---
+++ HTML&CSS/javascript/moduuli-02/example6/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/*
+
+Write a function that returns a random dice roll between 1 and 6.
+The function should not have any parameters. Write a main
+program that rolls the dice until the result is 6. The main
+program should print out the result of each roll in an unordered
+ list (<ul>). (2p)
+ */
+
+let roll;
+
+const selectedSlot = document.querySelector('.slot');
+
+
+
+  function diceRoll() {
+    roll = (parseInt(Math.floor(Math.random() * (7 - 1) + 1)));
+    return roll;
+  }
+  let result = diceRoll()
+  while (result !== 6){
+    result = diceRoll()
+    const listElement = document.createElement('li');
+    selectedSlot.appendChild(listElement);
+    listElement.innerHTML = result;
+  }
+
