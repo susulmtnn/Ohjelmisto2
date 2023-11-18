12cf7e7d33e77197bd1709c02e6ec218c66e9d2d
susulmtnn <suvi.liimatainen@gmail.com>
2023-11-18 21:17:08 +0200

    completed example 4

--- HTML&CSS/javascript/moduuli-01/example5/script.js
+++ HTML&CSS/javascript/moduuli-01/example5/script.js
@@ -3,12 +3,10 @@ let userInput = parseInt(prompt("Anna vuosiluku"));
 
 const selectedSlot= document.querySelector('.slot')
 
-if (userInput%100 !== 0 && userInput%4 === 0){
-  selectedSlot.innerHTML = 'karkausvuosi'}
-if (userInput%4 === 0 && userInput%400===0)
-{
-  selectedSlot.innerHTML ='karkausvuosi';
+if ((userInput%100 !== 0) && (userInput%4 === 0) || (userInput % 400 ==0)){
+  selectedSlot.innerHTML = 'leap year';
 }
+
 else {
-  selectedSlot.innerHTML= 'Ei karkausvuosi';
+  selectedSlot.innerHTML= 'Not a leap year';
 }
--- HTML&CSS/javascript/moduuli-02/example5/script.js
+++ HTML&CSS/javascript/moduuli-02/example5/script.js
@@ -17,35 +17,27 @@ let checker = true;
 
 let equal = false;
 
-console.log(lista.length);
 
   if (lista.length == 0) {
       lista.push(user_input);
-      console.log(lista, "this is lista's 0 element")
      }
 
 while (checker===true) {
   user_input = parseInt(prompt(
           "Please give numbers. Program will only stop if you enter a number that has already been entered"));
   for (let i = 0; i < lista.length; i++) {
-    console.log(i);
     if (user_input === lista[i]) {
-      console.log("equals");
       checker = false;
       equal=true;
       break;
-    } else {
-      console.log(user_input, lista[i]);
-      console.log(lista);
-      console.log("not equals");
     }
   }
   if (equal === true){
-    console.log(lista);
+    break;
   }
   else{
     lista.push(user_input);
-    console.log(lista);
   }
 }
-  console.log(lista, "final lista");
+  let sortedList = lista.sort((a, b) =>a-b);
+  console.log(sortedList);
