5291e0c5865e7b2f44dcbb1bbf0af826603cf9e5
susulmtnn <suvi.liimatainen@gmail.com>
2023-11-26 13:03:14 +0200

    completed example 4

--- HTML&CSS/javascript/moduuli3/module3-starters-main/t4/4.js
+++ HTML&CSS/javascript/moduuli3/module3-starters-main/t4/4.js
@@ -13,3 +13,16 @@ const students = [
     id: '5423679',
   },
 ];
+
+const selectedList = document.querySelector("#target");
+
+
+  console.log(students[0]);
+
+for (let i in students) {
+  const createdElement = document.createElement('option');
+  const person = students[i];
+  createdElement.textContent = person.name;
+  createdElement.value = person.id;
+  selectedList.appendChild(createdElement)
+}
