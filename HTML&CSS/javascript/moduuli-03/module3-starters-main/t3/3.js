374cdaf705a602f38df4034ce4b007316701d7c1
susulmtnn <suvi.liimatainen@gmail.com>
2023-11-27 20:58:40 +0200

    started example 1

--- HTML&CSS/javascript/moduuli-03/module3-starters-main/t3/3.js
+++ HTML&CSS/javascript/moduuli-03/module3-starters-main/t3/3.js
@@ -5,6 +5,6 @@ const selectedList = document.querySelector("#target");
 
 for (let i = 0; i< names.length; i++) {
   const createdElement = document.createElement('li');
-  createdElement.textContent = names[i];
+  createdElement.innerHTML = names[i];
   selectedList.appendChild(createdElement);
 }
