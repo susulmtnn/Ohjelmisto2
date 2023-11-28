fb8137afb4d730aa2e5ca55f06a134847ada42b4
susulmtnn <suvi.liimatainen@gmail.com>
2023-11-28 21:48:21 +0200

    I think finished example 1

--- HTML&CSS/javascript/moduuli-04/example1/example1.js
+++ HTML&CSS/javascript/moduuli-04/example1/example1.js
@@ -1,28 +1,2 @@
 'use strict'
 
-async function getQuery(query) {
-  try{
-    const response = await fetch('https://api.tvmaze.com/search/shows' + query);
-    const json = await response.json();
-    console.log(json.result);
-    return json;
-  }
-  catch (error){
-    console.error("Get query fetch error");
-  }
-}
-
-async function doQuery(event)
-    {
-      const selectedElem = document.querySelector('form').value;
-      const result = await getQuery(selectedElem)
-      console.log(selectedElem);
-      if (result.result){
-        console.log(selectedElem);
-      }
-      else{
-        console.log("fail" + result.result)
-      }
-    }
-
-document.querySelector('form').addEventListener('submit', doQuery);
