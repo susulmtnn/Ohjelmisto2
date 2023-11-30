63d9316be4029ba96856f0c4312001f1bc625239
susulmtnn <suvi.liimatainen@gmail.com>
2023-12-01 00:26:53 +0200

    example 3 works

--- HTML&CSS/javascript/moduuli-04/example3/example3.js
+++ HTML&CSS/javascript/moduuli-04/example3/example3.js
@@ -13,12 +13,71 @@ make <div id="results"> element to the HTML document where you append the <artic
 clear the old results with innerHTML = '' before you append the new results.
  */
 
+async function printSummary(response){
+  const selectedDiv = document.querySelector("#results");
+  try{
+      for (let i = 0; i < response.length; i++) {
+      const summary = response[i]['show']['summary'];
+      const selectedArticle = document.querySelector('#child'+ i.toString());
+      const createdDiv = document.createElement('div');
+      createdDiv.innerHTML = summary;
+      selectedArticle.appendChild(createdDiv);
+  }}catch(error){
+  console.log("some error");
+  const selectedBody = document.querySelector('body');
+  const createdDiv2 = document.createElement('div');
+  createdDiv2.innerHTML = error.message;
+  selectedBody.appendChild(createdDiv2);
+    }
+}
+
+async function printImage(response){
+  const selectedDiv = document.querySelector("#results");
+  try{
+      for (let i = 0; i < response.length; i++) {
+      const image = response[i].show.image?.medium;
+      const selectedArticle = document.querySelector('#child'+ i.toString());
+      const createdImage = document.createElement('img');
+      createdImage.src= image
+      createdImage.alt = response[i]['show']['name'];
+      selectedArticle.appendChild(createdImage);
+  }}catch(error){
+  console.log("some error");
+  const selectedBody = document.querySelector('body');
+  const createdDiv2 = document.createElement('div');
+  createdDiv2.innerHTML = error.message;
+  selectedBody.appendChild(createdDiv2);
+    }
+}
+
+async function printURL(response) {
+  const selectedDiv = document.querySelector("results");
+  try {
+    for (let i = 0; i < response.length; i++) {
+      const url = response[i]['show']['url']
+      console.log(url)
+      const selectedArticle = document.querySelector('#child'+ i.toString());
+      const createdAelement = document.createElement('a');
+      createdAelement.textContent = url.toString() + `target="_blank"`;
+      selectedArticle.appendChild(createdAelement);
+    }
+  }catch(error){
+  console.log("some error");
+  const selectedBody = document.querySelector('body');
+  const createdDiv2 = document.createElement('div');
+  createdDiv2.innerHTML = error.message;
+  selectedBody.appendChild(createdDiv2);
+  }
+}
+
+
+
 async function printName(response){
   console.log(response)
-  console.log(response[0]['show']['name'])
+  //console.log(response[0]['show']['name'])
   const selectedDiv = document.getElementById('results');
   try {
-    for (let i=0; i<= response.length; i++) {
+    for (let i=0; i< response.length; i++) {
         const createdArticle = document.createElement('article');
         const name = response[i]['show']['name'];
         console.log("this is name " + name);
@@ -53,6 +112,11 @@ async function getQuery(query){
 async function doQuery(event){
   event.preventDefault();
   const query = document.getElementById('query').value;
+  //check if the results already exist or not
+  let element = document.getElementById("results");
+  if (element !== null){
+    element.innerHTML = ""
+  }
   try{
   const response= await getQuery(query);
   console.log(response)
@@ -62,8 +126,15 @@ async function doQuery(event){
   createdDiv.id = 'results';
   selectedBody.appendChild(createdDiv);
   const nameDone = await printName(response);
+  const URLDone = await printURL(response);
+  const imageDone = await printImage(response);
+  const summaryDone = await printSummary(response);
   } catch (error){
-    console.log("some error");
+  console.log("some error");
+  const selectedBody = document.querySelector('body');
+  const createdDiv2 = document.createElement('div');
+  createdDiv2.innerHTML = error.message;
+  selectedBody.appendChild(createdDiv2);
   }
 }
 
